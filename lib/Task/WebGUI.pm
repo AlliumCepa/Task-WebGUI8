b4131ac96a6c13d18fa6159ef2d6c1f5421acc5a
Patrick Donelan <pdonelan@cpan.org>
2010-04-14 04:23:15 +0000

    Import of PDONELAN/Task-WebGUI-1.101040 from CPAN.
    gitpan-cpan-distribution: Task-WebGUI
    gitpan-cpan-version:      1.101040
    gitpan-cpan-path:         PDONELAN/Task-WebGUI-1.101040.tar.gz
    gitpan-cpan-author:       PDONELAN
    gitpan-cpan-maturity:     released

---
+++ lib/Task/WebGUI.pm
@@ -0,0 +1,46 @@
+# 
+# This file is part of Task-WebGUI
+# 
+# This software is copyright (c) 2010 by Patrick Donelan.
+# 
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+# 
+package Task::WebGUI;
+BEGIN {
+  $Task::WebGUI::VERSION = '1.101040';
+}
+# ABSTRACT: Install WebGUI dependencies
+
+
+1;
+__END__
+=pod
+
+=head1 NAME
+
+Task::WebGUI - Install WebGUI dependencies
+
+=head1 VERSION
+
+version 1.101040
+
+=head1 DESCRIPTION
+
+Run the following to install all WebGUI dependencies:
+
+ > cpanm Task::WebGUI
+
+=head1 AUTHOR
+
+  Patrick Donelan <pat@patspam.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Patrick Donelan.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
